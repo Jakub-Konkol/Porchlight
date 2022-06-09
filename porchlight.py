17187d38209a336b03cfaebaf96558de8511c687
Jakub-Konkol <101362884+Jakub-Konkol@users.noreply.github.com>
2022-06-09 17:11:05 -0400

    Copying over all code from the standalone Spyder project

---
+++ porchlight.py
@@ -0,0 +1,328 @@
+"""
+Created on Sat Apr  9 15:17:03 2022
+
+@author: Jakub
+"""
+# ===========================
+# Imports
+# ===========================
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+from tkinter import Menu
+from tkinter import messagebox as msg
+from tkinter import filedialog as fd
+from tkinter import Spinbox
+from time import sleep  # careful - this can freeze the GUI
+
+from functools import partial
+
+from spectralData import SpectralData
+from ToolTip import ToolTip
+
+
+# parameters = {}
+# funcCode = 'snv'
+# functions['funcCode'](**parameters)
+
+class PreprocessSelector(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.category_val = tk.StringVar()
+        self.method_val = tk.StringVar()
+        self.category = ttk.Combobox()
+
+        self.method = ttk.Combobox(left, width=12, textvariable=method_val)
+        self.method['values'] = ('Trim', 'Normalize', 'Smooth')
+        self.method.grid(column=0, row=0)
+
+        self.category = ttk.Combobox(left, width=12, textvariable=method_val)
+        self.category['values'] = ('Trim', 'Normalize', 'Smooth')
+        self.category.grid(column=1, row=0)
+
+
+class OOP():
+    def __init__(self):  # Initializer method
+        # Create instance
+        self.win = tk.Tk()
+
+        self.userData = SpectralData()
+
+        # Add a title
+        self.win.title("Porchlight")
+
+        self.categories = {'Trim': ['Trim', 'Inverse Trim'],
+                           # 'Baseline Correction': ['AsLS'],
+                           'Smoothing': ['Rolling', 'Savitzky-Golay'],
+                           'Normalization': ['SNV', 'MSC', 'Area', 'Peak Normalization', 'Vector', 'Min-max'],
+                           'Center': ['Mean', 'Last Point'],
+                           'Derivative': ['SG Derivative'],
+                           '': ''}
+
+        self.create_widgets()
+
+    def select_files(self):
+        filetypes = (
+            ('CSV Files', '*.csv'),
+            ('SPC Files', '*.spc'),
+            ('Text Files', '*.txt'),
+            ('All Files', '*.*'))
+
+        filenames = fd.askopenfilenames(
+            title='Select spectra files',
+            initialdir='/',
+            filetypes=filetypes)
+
+        # Check if files were selected and then load them into SpectralData
+        if not filenames:
+            print('No files selected')
+        else:
+            self.userData = SpectralData(filenames)
+            self.functions = {'reset': self.userData.reset,
+                              'Trim': self.userData.trim,
+                              'Inverse Trim': self.userData.invtrim,
+                              'Rolling': self.userData.rolling,
+                              'Savitzky-Golay': self.userData.SGSmooth,
+                              'SNV': self.userData.snv,
+                              'MSC': self.userData.msc,
+                              'Area': self.userData.area,
+                              'Peak Normalization': self.userData.peaknorm,
+                              'Vector': self.userData.vector,
+                              'Min-max': self.userData.minmax,
+                              'Mean': self.userData.mean_center,
+                              'Last Point': self.userData.lastpoint,
+                              'SG Derivative': self.userData.SGDeriv}
+            # 'AsLS': self.userData.asls,
+            self.plot_data()
+
+    def plot_data(self):
+        self.axis.clear()
+        self.axis.plot(self.userData.spc)
+
+        # make the tick labels bold
+        labels = self.axis.get_xticklabels() + self.axis.get_yticklabels()
+        for label in labels:
+            label.set_fontweight('bold')
+
+        spec_type = self.spectroscopy_type.get()
+
+        prefix = ''
+        invert_x = False
+
+        if spec_type:
+            if spec_type == 'IR - Transmission':
+                xlabel = 'Wavenumber (cm$^{-1}$)'
+                ylabel = 'Transmittance'
+                yunits = '(%)'
+                invert_x = True
+            elif spec_type == 'IR - Absorbance':
+                xlabel = 'Wavenumber (cm$^{-1}$)'
+                ylabel = 'Absorbance'
+                yunits = ''
+                invert_x = True
+            elif spec_type == 'Raman':
+                xlabel = 'Raman Shift (cm$^{-1}$)'
+                ylabel = 'Intensity'
+                yunits = '(counts)'
+            elif spec_type == 'UV-Vis':
+                xlabel = 'Wavelength, (nm)'
+                ylabel = 'Absorbance'
+                yunits = ''
+
+            norm_check = []
+            for ii in range(5):
+                if self.category[ii].get():
+                    norm_check.append(self.category[ii].get())
+
+            if 'Normalization' in norm_check or 'Center' in norm_check:
+                prefix = 'Normalized'
+                yunits = '(a.u.)'
+
+            if invert_x:
+                self.axis.invert_xaxis()
+
+            self.axis.set_xlabel(xlabel, fontweight="bold", fontsize=16)
+            self.axis.set_ylabel(prefix + ' ' + ylabel + ' ' + yunits, fontweight="bold", fontsize=16)
+        else:
+            self.axis.set_xlabel('Energy', fontweight="bold", fontsize=16)
+            self.axis.set_ylabel('Intensity (a.u.)', fontweight="bold", fontsize=16)
+
+        self.fig.tight_layout()
+
+        self.canvas.draw()
+
+    def update_methods(self, event, value):
+        self.method[value].set('')
+        self.method[value]['values'] = tuple(self.categories[event.widget.get()])
+
+    def perform_preprocessing(self, event):
+        self.userData.reset()
+        for ii in range(5):
+            if self.method[ii].get():
+                if bool(self.optA[ii].get()) & bool(self.optB[ii].get()) & bool(self.optC[ii].get()):
+                    param = (float(self.optA[ii].get()), float(self.optB[ii].get()), float(self.optC[ii].get()))
+                elif bool(self.optA[ii].get()) & bool(self.optB[ii].get()):
+                    param = (float(self.optA[ii].get()), float(self.optB[ii].get()))
+                elif bool(self.optA[ii].get()):
+                    param = (float(self.optA[ii].get()),)
+                else:
+                    param = ()
+
+                self.functions[self.method[ii].get()](*param)
+        self.plot_data()
+
+    # update progressbar in callback loop
+    def run_progressbar(self):
+        self.progress_bar["maximum"] = 100
+        for i in range(101):
+            sleep(0.05)
+            self.progress_bar["value"] = i  # increment progressbar
+            self.progress_bar.update()  # have to call update() in loop
+        self.progress_bar["value"] = 0  # reset/clear progressbar
+
+    def start_progressbar(self):
+        self.progress_bar.start()
+
+    def stop_progressbar(self):
+        self.progress_bar.stop()
+
+    def progressbar_stop_after(self, wait_ms=1000):
+        self.win.after(wait_ms, self.progress_bar.stop)
+
+        # Exit GUI cleanly
+
+    def _quit(self):
+        self.win.quit()
+        self.win.destroy()
+        exit()
+
+        #####################################################################################
+
+    def create_widgets(self):
+
+        # set up the left/right frames
+        self.left = ttk.Frame(self.win)
+        self.left.grid(column=0, row=0, padx=8, pady=4)
+        self.right = ttk.LabelFrame(self.win)
+        self.right.grid(column=1, row=0, padx=8, pady=4)
+
+        # set up the three panels of left
+        setup_section = ttk.LabelFrame(self.left, text='Spectroscopy Type and Load')
+        parameter_section = ttk.LabelFrame(self.left, text='Parameter Selection')
+        export_section = ttk.LabelFrame(self.left, text='Export')
+
+        setup_section.grid(column=0, row=0)
+        parameter_section.grid(column=0, row=1)
+        export_section.grid(column=0, row=2)
+
+        # making the spectroscopy type combobox
+        self.spectroscopy_type = ttk.Combobox(setup_section, width=15, state='readonly')
+        self.spectroscopy_type['values'] = ('IR - Transmission', 'IR - Absorbance', 'Raman', 'UV-Vis')
+        self.spectroscopy_type.grid(column=0, row=0)
+
+        # making the load files button
+        open_button = ttk.Button(
+            setup_section,
+            text='Load Files',
+            command=self.select_files)
+
+        open_button.grid(column=0, row=1)
+
+        # Prepare the variables for the preprocessing selectors
+        self.category_select = [tk.StringVar()] * 5
+        self.method_select = [tk.StringVar()] * 5
+        self.optA_val = [tk.StringVar()] * 5
+        self.optB_val = [tk.StringVar()] * 5
+        self.optC_val = [tk.StringVar()] * 5
+
+        self.optA = {}
+        self.optB = {}
+        self.optC = {}
+        self.category = {}
+        self.method = {}
+
+        # This is the preprocessing rows
+        for ii in range(5):
+            label_0 = ttk.Label(parameter_section, text="Preprocessing " + str(ii + 1) + ": ")
+            label_0.grid(column=0, row=ii + 1)
+
+            category = ttk.Combobox(parameter_section, width=15, state='readonly')
+            category['values'] = tuple(self.categories.keys())
+            category.grid(column=1, row=ii + 1)
+            category.bind("<<ComboboxSelected>>", lambda x, i=ii: self.update_methods(x, i))
+
+            self.category[ii] = category
+
+            method = ttk.Combobox(parameter_section, width=15, state='readonly')
+            method.grid(column=2, row=ii + 1)
+            method.bind("<<ComboboxSelected>>", lambda x: self.perform_preprocessing(x))
+
+            self.method[ii] = method
+
+            optA = ttk.Entry(parameter_section, width=5)
+            optA.grid(column=3, row=ii + 1)
+            optA.bind("<FocusOut>", self.perform_preprocessing)
+
+            optB = ttk.Entry(parameter_section, width=5)
+            optB.grid(column=4, row=ii + 1)
+            optB.bind("<FocusOut>", self.perform_preprocessing)
+
+            optC = ttk.Entry(parameter_section, width=5)
+            optC.grid(column=5, row=ii + 1)
+            optC.bind("<FocusOut>", self.perform_preprocessing)
+
+            self.optA[ii] = optA
+            self.optB[ii] = optB
+            self.optC[ii] = optC
+
+        self.right_label = ttk.Label(self.right, text='Data Preview')
+        self.right_label.grid(column=0, row=0)
+
+        # generate the figure
+        self.fig = Figure(figsize=(6, 4))
+        self.axis = self.fig.add_subplot(111)
+
+        self.axis.tick_params(axis="y", direction="in", labelsize=14)
+        self.axis.tick_params(axis="x", direction="in", labelsize=14)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.right)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.right, pack_toolbar=False)
+        self.toolbar.update()
+        self.canvas._tkcanvas.grid(column=0, row=1)
+        self.toolbar.grid(column=0, row=2, sticky=tk.W)
+        # Creating a Menu Bar
+        menu_bar = Menu(self.win)
+        self.win.config(menu=menu_bar)
+
+        # Add menu items
+        file_menu = Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="New")
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self._quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+
+        # Display a Message Box
+        def _msgBox():
+            msg.showinfo('Python Message Info Box', 'A Python GUI created using tkinter:\nThe year is 2022.')
+
+            # Add another Menu to the Menu Bar and an item
+
+        help_menu = Menu(menu_bar, tearoff=0)
+        help_menu.add_command(label="About", command=_msgBox)  # display messagebox when clicked
+        menu_bar.add_cascade(label="Help", menu=help_menu)
+
+        # Change the main windows icon
+        self.win.iconbitmap('porchlight.ico')
+
+
+# ======================
+# Start GUI
+# ======================
+oop = OOP()
+oop.win.mainloop()
+
+
---
+++ spectralData.py
@@ -0,0 +1,341 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 21:23:25 2022
+
+@author: Jakub
+"""
+
+class SpectralData():
+    def __init__(self, file=None):
+        import numpy as np
+        import pandas as pd
+        import re
+        import os
+
+        if file is None:
+            self._wav_raw = [0]
+            self._spc_raw = [0]
+            self.war = [0]
+            self.spc = [0]
+            self.wav = [0]
+        else:
+            #initialize temporary arrays
+            store_spc = np.array([])
+            self.file_source = []
+            self.tf_history = []
+
+            #start with first file
+            if isinstance(file, str):
+                wav, spc = self.getDataFromFile(file)
+            else:
+                wav, spc = self.getDataFromFile(file[0])
+
+
+            if spc.ndim == 1:
+                store_spc = np.concatenate((store_spc, spc))
+            elif spc.ndim == 2:
+                store_spc = np.concatenate((store_spc, spc[:,0]))
+                store_spc = np.column_stack((store_spc, spc[:,1:]))
+
+            self._wav_raw = wav
+            self.wav = self._wav_raw
+
+            if not isinstance(file, str) and len(file)>1:
+                for ii in range(1, len(file)):
+                    wav, spc = self.getDataFromFile(file[ii])
+                    store_spc = np.column_stack((store_spc, spc))
+                    #TODO: what if wav is not the same? some kind of pandas merge
+
+            self._spc_raw = pd.DataFrame(store_spc, index=self.wav)
+
+            self.spc = self._spc_raw
+            self.war = np.zeros((self.spc.shape[1]))
+
+
+    def getDataFromFile(self, file):
+        """
+        Perform logic to determine filetype and corresponding import function. Import data and append file to imported_file array to keep track.
+
+        Parameters
+        ----------
+        file : STR
+            Path to file.
+
+        Returns
+        -------
+        wav : NP.ARRAY
+            Array of independent wavenumber values
+        spc : NP.ARRAY
+            Array of spectral values, ordered as one spectra per column
+
+        """
+        import os
+        ext = os.path.splitext(file)[-1].lower()
+        #go through decision tree to get correct import function
+        if ext == '.txt':
+            wav, spc = self.readTextFile(file)
+        elif ext == ".spc":
+            wav, spc = self.readSPCFile(file)
+        elif ext == '.jdx':
+            print("J-Camp file import not implemented yet")
+            #TODO: figure out j-camp import
+        elif ext == '.csv':
+            wav, spc = self.readCSVFile(file)
+        else:
+            print("This filetype is not supported")
+
+        if spc.ndim == 1:
+            self.file_source.append(os.path.split(file)[1])
+        elif spc.ndim == 2:
+            self.file_source = self.file_source + [os.path.split(file)[1] for x in range(spc.shape[1])]
+
+        return wav, spc
+
+    def readTextFile(self, file):
+        """
+        Perform interpretation for txt and csv files.
+
+        Parameters
+        ----------
+        file : STR
+            The path to the file
+
+        Returns
+        -------
+        wav : NP.ARRAY
+            Array of independent wavenumber values
+        spc : NP.ARRAY
+            Array of spectral values, ordered as one spectra per column
+
+        """
+        import numpy as np
+        data = np.loadtxt(file)
+        #assuming that there are more data points than spectra, make columnwise
+        if data.shape[0] < data.shape[1]:
+            data = data.transpose()
+
+        wav = data[:,0]
+        spc = data[:,1:]
+
+        #return squeezed spc in case only 1D
+        return wav, np.squeeze(spc);
+
+    def readCSVFile(self, file):
+        """
+        Perform interpretation for txt and csv files.
+
+        Parameters
+        ----------
+        file : STR
+            The path to the file
+
+        Returns
+        -------
+        wav : NP.ARRAY
+            Array of independent wavenumber values
+        spc : NP.ARRAY
+            Array of spectral values, ordered as one spectra per column
+
+        """
+        import numpy as np
+        data = np.loadtxt(file, delimiter = ',')
+        #assuming that there are more data points than spectra, make columnwise
+        if data.shape[0] < data.shape[1]:
+            data = data.transpose()
+
+        wav = data[:,0]
+        spc = data[:,1:]
+
+        #return squeezed spc in case only 1D
+        return wav, np.squeeze(spc);
+
+    def readSPCFile(self, file):
+        try:
+            import spc_spectra
+        except:
+            print("Porchlight can use spc_spectra to import spc files.\nInstall using 'pip install spc_spectra'")
+            return
+        import numpy as np
+
+        f = spc_spectra.File(file)
+
+        #we can use the position of the '-' to figure out how to handle the x and y
+        pos =  f.dat_fmt.find('-')
+        if pos == 0:
+            # code is of type -xy, assume all have the same wav
+            wav = f.sub[0].x
+        elif pos == 1:
+            # code is of type -xy
+            wav = f.x
+        elif pos == 2:
+            # code is of type gx-y
+            wav = f.x
+
+        temp=[]
+        for ii in range(f.fnsub):
+            temp.append(f.sub[ii].y)
+        spc = np.array(temp).transpose()
+        return wav, np.squeeze(spc)
+
+    def plot(self):
+        self.spc.plot()
+
+    def reset(self):
+        """
+        Reset the spectra and wavenumber to raw imported values.
+        """
+        self.spc = self._spc_raw
+        self.wav = self._wav_raw
+
+    def rolling(self, window):
+        if window is None:
+            print("Warning: no window length supplied. Defaulting to 1.")
+            window=1
+        if not isinstance(window, int):
+            window = int(window)
+
+        #first perform the rolling window smooth
+        self.spc = self.spc.rolling(window).mean()
+
+        #use the NANs to cut the wav and spc matrices
+        self.wav = self.wav[self.spc.iloc[:,0].notna()]
+        self.spc = self.spc.dropna()
+
+    def SGSmooth(self, window, poly):
+        """
+        Performs Savitzky-GOlay smoothing of the data as implemented by scipy.signal.
+
+        Parameters
+        ----------
+        window : INT
+            The window size, or number of points used in the fitting.
+        poly : INT
+            Order of polynomial used in the fitting.
+
+        Returns
+        -------
+        None.
+
+        """
+        from scipy.signal import savgol_filter
+        import pandas as pd
+
+        if not isinstance(window, int):
+            window = int(window)
+        if not isinstance(poly, int):
+            poly = int(poly)
+
+        self.spc = pd.DataFrame(savgol_filter(self.spc, window, poly, axis = 0), columns = self.spc.columns, index = self.spc.index)
+
+    def SGDeriv(self, window, poly, order):
+        from scipy.signal import savgol_filter
+        import pandas as pd
+
+        if not isinstance(window, int):
+            window = int(window)
+        if not isinstance(poly, int):
+            poly = int(poly)
+
+        self.spc = pd.DataFrame(savgol_filter(self.spc, window, poly, order, axis = 0), columns = self.spc.columns, index = self.spc.index)
+
+    def snv(self):
+        """
+        Performs in-place SNV normalization of data.
+
+        Returns
+        -------
+        None.
+
+        """
+        import numpy as np
+        self.spc = self.spc.apply(lambda x: (x-np.mean(x))/np.std(x), axis = 0)
+
+    def msc(self, reference=None):
+        """
+        Performs in-place MSC scatter correction of data.
+
+        Parameters
+        ----------
+        reference : INT, optional
+            The value of the spectra to be fitted to. If none is provided, the average spectrum is used. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
+        import numpy as np
+
+        if reference == None:
+            ref = self.spc.mean(axis=1)
+        else:
+            if isinstance(reference, int):
+                ref = self.spc.iloc[:,reference]
+            else:
+                reference = int(reference)
+                ref = self.spc.iloc[:,reference]
+
+        for ii in range(self.spc.shape[1]):
+            fit = np.polyfit(ref, self.spc.iloc[:,ii], 1, full=True)
+            self.spc.iloc[:,ii] = (self.spc[ii] - fit[0][1])/fit[0][0]
+
+    def trim(self, start, end):
+        if start is None:
+            start = self.spc.index[0]
+        if end is None:
+            end = self.spc.index[-1]
+
+        self.spc = self.spc.loc[(self.spc.index > start) & (self.spc.index < end)]
+        self.wav = self.wav[(self.wav > start) & (self.wav < end)]
+
+    def invtrim(self, start, end):
+        if start is None:
+            start = self.spc.index[0]
+        if end is None:
+            end = self.spc.index[-1]
+
+        self.spc = self.spc.loc[(self.spc.index < start) | (self.spc.index > end)]
+        self.wav = self.wav[(self.wav < start) | (self.wav > end)]
+
+    def area(self):
+        import numpy as np
+        self.spc = self.spc / self.spc.apply(lambda x: np.trapz(x, self.wav), axis=0)
+
+    def lastpoint(self):
+        self.spc = self.spc.sub(self.spc.iloc[-1,:])
+    def mean_center(self):
+        self.spc = self.spc.sub(self.spc.mean(axis=0))
+    def peaknorm(self, wavenumber):
+        index = self.spc.index.get_loc(wavenumber, method='nearest')
+        self.spc = self.spc.divide(self.spc.iloc[index,:])
+    def vector(self):
+        self.spc = self.spc.divide( ((self.spc**2).sum(axis=0))**(1/2))
+    def minmax(self, min_val = 0, max_val = 1):
+        self.spc = min_val + (self.spc.sub(self.spc.min(axis=0))) * (max_val - min_val) / (self.spc.max(axis=0) - self.spc.min(axis=0))
+    def _get_AsLS_baseline(self, y, lam, p, niter):
+        #adapted from https://stackoverflow.com/a/50160920
+        import numpy as np
+        from scipy import sparse
+        from scipy.sparse.linalg import spsolve
+
+        L = self.spc.shape[0]
+        D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
+        w = np.ones(L)
+        for i in range (niter):
+            W = sparse.spdiags(w, 0, L, L)
+            Z = W + lam * D.dot(D.transpose())
+            z = spsolve(Z, w*y)
+            w = p * (y > z) + (1 - p) * (y < z)
+        return z
+
+import os
+# import cProfile
+
+# files = os.listdir("spectra")
+# files = ["./spectra/" + x for x in files]
+# testData = SpectralData(files)
+
+file_spc = 'C:/Users/Jakub/OneDrive/Documents/Rutgers/Tsilomelekis/Data/Bioreactor/Bioreactor/P1BR2/Probe 1_P1BR2 1075 15m_20191112-080528/P1BR2 1075 15m_20191112-081817.spc'
+spc_test = SpectralData((file_spc, ))
+
+#nirData = SpectralData(("C:/Users/Jakub/Desktop/NIRSpectra_noHeader.txt",))
